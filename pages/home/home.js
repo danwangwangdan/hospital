3595edc435762830a7535dc3496a3054a2b408d6
hsm <904837992@.com>
2018-10-23 16:57:25 +0800

    页面页面。。。

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,7 +2,7 @@
 const app = getApp();
 Page({
     data: {
-        troubleType: ['电脑问题', '导航台软件使用问题', '打印机问题', '其他问题'],
+      troubleType: ['导航台问题', '电脑问题', '打印机问题', '其他问题'],
         index: 0,
     },
 
