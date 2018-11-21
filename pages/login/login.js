2fef8f4b9ef922d14ff41c1d8238ddadd07f735e
hsm <904837992@qq.com>
2018-11-21 23:32:27 +0800

    登录逻辑

--- pages/login/login.js
+++ pages/login/login.js
@@ -9,24 +9,45 @@ Page({
   },
     //事件处理函数
     toLogin: function () {
-        wx.request({
-            url: app.globalData.localApiUrl + '/user/login',
-            data: {
-                "username": this.data.username,
-                "password": this.data.password
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                console.log(res.data);
-        wx.switchTab({
-            url: '/pages/submit/submit'
-        });
-
-            }
-        });
-
+        var username = this.data.username;
+        var password = this.data.password;
+        // 非空检验
+        if (!username || !password) {
+            wx.showToast({
+                title: '账号或密码不能为空',
+                icon: 'none',
+                duration: 2000
+            });
+        } else {
+            wx.request({
+                url: app.globalData.localApiUrl + '/user/login',
+                data: {
+                    "username": username,
+                    "password": password
+                },
+                header: {
+                    'content-type': 'application/json'
+                },
+                success(res) {
+                    console.log(res.data);
+                    if (res.data.code == 1) {
+                        // 登录成功
+                        wx.switchTab({
+                            url: '/pages/submit/submit'
+                        });
+                        // 存储用户信息
+                        app.globalData.userInfo = JSON.parse(res.data.data);
+                    } else {
+                        // 登录失败
+                        wx.showToast({
+                            title: '登录失败，账号或密码错误',
+                            icon: 'none',
+                            duration: 2000
+                        })
+                    }
+                }
+            });
+        }
     },
     getUserInfo: function (e) {
     console.log(e)
