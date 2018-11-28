5218233102c2b8889bf8cc1aead1f67d3ce0d4db
hsm <904837992@qq.com>
2018-11-28 23:15:54 +0800

    接口调试

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,12 +2,62 @@
 const app = getApp();
 Page({
   data: {
-    troubleType: ['电脑问题', '导航台软件使用问题', '打印机问题', '其他问题'],
-    index: 0,
+      troubleId: 0,
+      isCommitted: "",
+      isConfirmed: "process",
+      isSolved: "",
+      captureUrl: "",
+      detail: "",
+      secType: "",
+      firType: "",
+      office: "",
+      submitTime: "",
+      troubleOwner: ""
+  },
+    onLoad: function (options) {
+        var troubleId = 1;
+        this.setData({
+            troubleId: troubleId
+        });
+        wx.request({
+            url: app.globalData.localApiUrl + '/trouble/detail?troubleId=' + troubleId,
+            method: 'GET',
+            success(res) {
+                console.log(res.data);
+                if (res.data.code == 1) {
+                    var trouble = res.data.data;
+                    this.setData({
+                        troubleOwner: trouble.troublePersonName,
+                        submitTime: trouble.submitTime,
+                        office: trouble.office,
+                        firType: trouble.firType,
+                        secType: trouble.secType,
+                        detail: trouble.detail,
+                        captureUrl: trouble.captureUrls
+                    })
+                    if (trouble.status == 1) {
+                        this.setData({
+                            isCommitted: 'process',
+                            isSolved: "",
+                            isConfirmed: ""
+                        });
+                    } else if (trouble.status == 2) {
+                        this.setData({
+                            isSolved: "",
+                            isConfirmed: 'process',
+                            isCommitted: '',
+                        });
+                    } else {
+                        this.setData({
+                            isConfirmed: "",
+                            isCommitted: '',
+                            isSolved: 'process'
+                        });
+                    }
+
+                }
+            }
+        });
   },
 
-
-  onLoad: function() {
-
-  },
 })
