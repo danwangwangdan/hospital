8b1cb3d488b623ea0a5d23abad47bfb455b69e07
hsm <904837992@qq.com>
2018-12-14 23:52:04 +0800

    接口

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 App({
   globalData: {
     userInfo: null,
-    //localApiUrl: "http://localhost/hishelp",
+    //localApiUrl: "http://192.168.0.102/hishelp",
     localApiUrl: "https://www.daliandaxue.cn/hishelp",
     remoteApiUrl: "",
   },
@@ -19,7 +19,7 @@ Date.prototype.format = function(fmt) {
     "m+": this.getMinutes(), //分
     "s+": this.getSeconds(), //秒
     "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-    "S+": this.getMilliseconds() //毫毛
+    "S+": this.getMilliseconds() //毫秒
   };
   if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
   for (var k in o)
