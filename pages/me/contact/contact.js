ce91fde683518dacb8c1f247760e85a7638bb749
hsm <904837992@qq.com>
2018-12-19 21:49:32 +0800

    接口

---
+++ pages/me/contact/contact.js
@@ -0,0 +1,75 @@
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    contactList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onShow: function(options) {
+    var that = this;
+    wx.request({
+      url: app.globalData.localApiUrl + '/common/contact',
+      method: 'GET',
+      success(res) {
+        console.log(res.data);
+        if (res.data.code == 1) {
+          var data = res.data.data;
+          that.setData({
+            contactList: data
+          });
+        }
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
